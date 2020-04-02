5256dc40bb47ef16d9edbd21694290df479f9449
Shawn Wright <volushawn@gmail.com>
2020-04-02 00:00:17 -0500

    readme

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Employee = (props) => {
     const employees = props.persons.map((employee) =>
         <div key={employee.id.value} className="col-lg-3 col-md-4 col-sm-12 my-4 mx-4">
-            <div className="card" style={{ width: "18rem" }}>
+            <div className="card shadow" style={{ width: "19rem" }}>
                 <img src={employee.picture.large} className="card-img-top" alt="" />
-                <div className="card-body">
+                <div className="card-body shadow">
                     <h5 className="card-title">{employee.name.first} {employee.name.last}</h5>
                     <p className="card-text">Office: {employee.location.city}, {employee.location.state}</p>
                     <p className="card-text">Username: {employee.login.username}</p>
